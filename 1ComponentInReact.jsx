2c7a665f9ba523213fe09ca1532edbe29f7c631a
amitdhamsaniya <dhamsaniyaamit@gmail.com>
2023-12-12 16:45:26 +0530

    data

---
+++ 1ComponentInReact.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComponentInReact extends Component {
+    render() {
+        return (
+            <>
+              <p>componenet in react</p>
+            </>
+        );
+    }
+}
+
+export default ComponentInReact;
