3db3472b9703aa45a2e4be821b11dd5074397437
amitdhamsaniya <dhamsaniyaamit@gmail.com>
2024-01-22 15:19:50 +0530

    data

--- src/functional component/16USeContaxt/usecontext page/UseContextFunctionalComponentInReact.jsx
+++ src/functional component/16USeContaxt/usecontext page/UseContextFunctionalComponentInReact.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import NoteContext from './NoteContext.jsx'
 const UseContextFunctionalComponentInReact = (props) => {
-const state = {
-    "name":"amit",
-    "class":"science"
-}
+
     return (
         <>
-            <NoteContext.provider vaule={state}>
-                {props.children}
-            </NoteContext.provider>
-            <NoteContext />
+       <div className="alert alert-info">
+        <div>
+            <h2>syntax</h2>
+            <p>const authContext = useContext(initialValue);</p>
+        </div>
+        <p>Context provides a way to pass data or state through the component tree without having to pass props down manually through each nested component</p>
+       </div>
         </>
     );
 };
